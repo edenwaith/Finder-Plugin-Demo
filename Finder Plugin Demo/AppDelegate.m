b750589da29306b233528f752e29aa7d55f1cd9a
Chad Armstrong <chad@edenwaith.com>
2014-04-21 20:27:22 -0500

    Initial Commit

---
+++ Finder Plugin Demo/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Finder Plugin Demo
+//
+//  Created by Chad Armstrong on 4/21/14.
+//  Copyright (c) 2014 Edenwaith. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ Finder Plugin Demo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Finder Plugin Demo
+//
+//  Created by Chad Armstrong on 4/21/14.
+//  Copyright (c) 2014 Edenwaith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
